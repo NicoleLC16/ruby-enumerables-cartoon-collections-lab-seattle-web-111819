adc1fe157f2becc3dd45294e9c79dffcd1cfd7f7
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 00:20:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(dwarves)
   puts dwarves_call.map {|name, index|"#{index+1}. #{name}"}
   end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.
 end
 
 def long_planeteer_calls# code an argument here
