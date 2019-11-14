c5276346efb053c701bf220efb3fc71d854b65b4
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 01:01:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,12 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find do |word| 
-    word.size > 4
-    puts true
-    word.size < 4 == false
-    puts false
-  end
+  array.any? {|word| word.size > 4}
   
 end
 
