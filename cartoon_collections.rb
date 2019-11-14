3a88ee425433c42369fe9e69a77f47c43a182b0e
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-14 00:00:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index{|name| puts name, "1.\n"}
+  dwarves.each_with_index{|name| puts name, "/n"}
 end
 
 def summon_captain_planet# code an argument here
